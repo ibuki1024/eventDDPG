39aa24cd3a2d695bf6e195fee99382c8a9b5abb8
Ibuki Takeuchi <ibuki@hacarus.com>
2019-09-22 18:07:42 +0900

    all file upload

---
+++ module/rl/agents/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from .dqn import DQNAgent, NAFAgent, ContinuousDQNAgent
+from .ddpg import DDPGAgent
+from .cem import CEMAgent
+from .sarsa import SarsaAgent, SARSAAgent
+from .eventddpg import eventDDPGAgent
